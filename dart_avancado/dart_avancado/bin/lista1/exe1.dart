ce39e7e0cdf64f599fbfde676a220ea3289594cf
Leonardo Fernandes <leonardo.fernandes@PROWAY.TREINA>
2023-11-09 20:44:55 -0300

    adicionado exercicio 2 exceptions

--- dart_avancado/dart_avancado/bin/lista1/exe1.dart
+++ dart_avancado/dart_avancado/bin/lista1/exe1.dart
@@ -1,13 +1,12 @@
-
 import 'dart:io';
 
 void main(List<String> args) {
   List<int> listaNumeros = [];
 
   try {
-    while(true){
+    while (true) {
       print('Informe um número inteiro:');
-      listaNumeros.add( int.parse(stdin.readLineSync()!) );
+      listaNumeros.add(int.parse(stdin.readLineSync()!));
     }
   } catch (e) {
     print(listaNumeros);
