25e552301bd1b906d3dfed280d64d2af292d1fdd
Leonardo Fernandes <leonardo.fernandes@PROWAY.TREINA>
2023-11-09 19:54:10 -0300

    Adicionado dart avancado

---
+++ dart_avancado/dart_avancado/bin/lista1/exe1.dart
@@ -0,0 +1,15 @@
+
+import 'dart:io';
+
+void main(List<String> args) {
+  List<int> listaNumeros = [];
+
+  try {
+    while(true){
+      print('Informe um número inteiro:');
+      listaNumeros.add( int.parse(stdin.readLineSync()!) );
+    }
+  } catch (e) {
+    print(listaNumeros);
+  }
+}
